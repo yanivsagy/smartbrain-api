052c028025975811bed568be5de03a713484ef86
Yaniv Sagy <yanivsagy@gmail.com>
2021-10-05 18:14:08 -0700

    .env updated

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: '0b8f12baadf14e34be4e303e7ca3fca5'
+    apiKey: process.env.CLARIFAI_API_KEY
   });
 
 const handleApiCall = (req, res) => {
